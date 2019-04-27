e6985dba1ade9084cf164c965c24e4d7e734357d
Igor Nikitin <rabbitinspace@icloud.com>
2019-04-27 20:17:22 +1000

    sql for schedule table

---
+++ migrations/2019-04-27-083905_initial/up.sql
@@ -0,0 +1,11 @@
+create table schedule
+(
+    id       integer not null
+        constraint schedule_pk
+            primary key autoincrement,
+    anime_id integer not null,
+    state    integer default 0 not null
+);
+
+create unique index schedule_anime_id_uindex
+    on schedule (anime_id);
