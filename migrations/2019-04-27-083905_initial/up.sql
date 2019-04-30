942985cf499919cc0a0dd7ad499d165a7bddc932
Igor Nikitin <rabbitinspace@icloud.com>
2019-04-30 22:15:39 +1000

    update schedule db model

--- migrations/2019-04-27-083905_initial/up.sql
+++ migrations/2019-04-27-083905_initial/up.sql
@@ -1,23 +1,34 @@
 create table schedules
 (
-    id          integer not null
+    id                integer not null
         constraint schedules_pk
             primary key autoincrement,
 
-    anidb_id   integer not null,
-    state      integer default 0 not null,
-    data_mask1 integer default 0 not null,
-    created_at double default current_timestamp not null,
-    updated_at double default current_timestamp not null
+    anidb_id          integer not null,
+    state             integer default 0 not null,
+    priority          integer default 0 not null,
+    has_poster        integer default 0 not null,
+    has_air_date      integer default 0 not null,
+    has_type          integer default 0 not null,
+    has_mal_id        integer default 0 not null,
+    has_tags          integer default 0 not null,
+    has_episode_count integer default 0 not null,
+    has_episodes      integer default 0 not null,
+    has_rating        integer default 0 not null,
+    has_description   integer default 0 not null,
+    created_at        double  default current_timestamp not null,
+    updated_at        double  default current_timestamp not null
 );
 
 create unique index schedules_anidb_id_uindex
     on schedules (anidb_id);
 
 create trigger schedules_updated_at_trigger
-    after update on schedules
+    after update
+    on schedules
     for each row
     begin
-        update schedules set updated_at = current_timestamp
-            where id = old.id;
-end;
+        update schedules
+        set updated_at = current_timestamp
+        where id = old.id;
+    end;
