157b74fc771336238807e9eab57d6d98c7120184
Igor Nikitin <rabbitinspace@icloud.com>
2019-05-06 23:38:30 +1000

    unit tests for file backup future

--- src/worker/dump/copy.rs
+++ src/worker/dump/copy.rs
@@ -21,3 +21,71 @@ where
         })
         .map(|_| ())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::{self, File};
+    use std::io::{Read, Write};
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_copy() {
+        let (src_path, dst_path) = create_file_paths("rw");
+        let content = b"Hello there".to_vec();
+
+        File::create(&src_path)
+            .and_then(|mut f| f.write_all(&content))
+            .expect("failed to write test data");
+
+        let fut =
+            copier(src_path, dst_path.clone()).map_err(|e| panic!("failed to copy data: {}", e));
+
+        tokio::run(fut);
+
+        let mut written_content = vec![];
+        File::open(dst_path)
+            .and_then(|mut f| f.read_to_end(&mut written_content))
+            .expect("failed to read data from copied file");
+
+        assert_eq!(content, written_content);
+    }
+
+    #[test]
+    fn test_no_copy_needed() {
+        let (src_path, dst_path) = create_file_paths("no");
+        let fut = copier(src_path.clone(), dst_path.clone());
+
+        tokio::run(futures::future::lazy(move || {
+            fut.then(|res| {
+                match res {
+                    Ok(_) => panic!("expected to receive NotFound error"),
+                    Err(e) => assert_eq!(e.kind(), std::io::ErrorKind::NotFound),
+                }
+
+                Ok(())
+            })
+        }));
+
+        assert!(!src_path.exists(), "no files should be created");
+        assert!(!dst_path.exists(), "no files should be created");
+    }
+
+    fn create_file_paths(suffix: &str) -> (PathBuf, PathBuf) {
+        let mut src_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        src_path.push(format!("resources/tests/copy_me{}", suffix));
+
+        let mut dst_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        dst_path.push(format!("resources/tests/copy_into_me{}", suffix));
+
+        if src_path.exists() {
+            fs::remove_file(&src_path).expect("failed to clean tests dir");
+        }
+
+        if dst_path.exists() {
+            fs::remove_file(&dst_path).expect("failed to clean tests dir");
+        }
+
+        (src_path, dst_path)
+    }
+}
