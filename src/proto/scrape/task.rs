a446a40f9b0235e8f9928116a54ac2bf5233301f
Igor Nikitin <rabbitinspace@icloud.com>
2019-05-31 01:38:45 +1000

    update protos

--- src/proto/scrape/task.rs
+++ src/proto/scrape/task.rs
@@ -1,15 +1,15 @@
 /// Represents a task for anime pages scraping
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Task {
-    /// Task id
+    /// Task ID
     #[prost(string, tag="1")]
     pub id: std::string::String,
     /// Type of an anime info source to parse from
     #[prost(enumeration="task::Source", tag="2")]
     pub source: i32,
-    /// Anime ids
+    /// Schedule IDs
     #[prost(sint32, repeated, tag="3")]
-    pub anime_ids: ::std::vec::Vec<i32>,
+    pub schedule_ids: ::std::vec::Vec<i32>,
 }
 pub mod task {
     /// External DB where an anime info resides
@@ -25,8 +25,11 @@ pub struct TaskYield {
     /// ID of the related task
     #[prost(string, tag="1")]
     pub task_id: std::string::String,
-    /// Parsed anime entities
-    #[prost(message, repeated, tag="2")]
+    /// ID of the schedule
+    #[prost(sint32, tag="2")]
+    pub schedule_id: i32,
+    /// Parsed anime entity
+    #[prost(message, repeated, tag="3")]
     pub anime: ::std::vec::Vec<super::anime::Anime>,
 }
 /// Signals that a task has been finished
