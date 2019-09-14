7801cc7e360b00d29dcb5c98cdb9827f920e3d65
Igor Nikitin <rabbitinspace@icloud.com>
2019-09-14 17:26:15 +1000

    separate settings for integration tests

--- src/settings.rs
+++ src/settings.rs
@@ -1,11 +1,12 @@
 use config::{Config, ConfigError, File};
 use lazy_static::lazy_static;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 use std::time::Duration;
 
 lazy_static! {
-    static ref SHARED: Settings = Settings::new().expect("failed to read settings");
+    static ref SHARED: Settings = Settings::new(Profile::Default).expect("failed to read settings");
 }
 
 /// Returns reference to global settings instance
@@ -13,80 +14,105 @@ pub fn shared() -> &'static Settings {
     &SHARED
 }
 
+/// Settings profile
+///
+/// Each enum case represents a file in `config` directory.
+/// All profiles are based on `Default`, that is, all settings from `Default`
+/// profile will be available, but may be overridden, will be inherited
+/// by other profiles.
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum Profile {
+    Default,
+    Test,
+}
+
 /// Application settings
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Settings {
-    db: Db,
-    import: Import,
-    ports: Ports,
+    /// Database settings
+    pub db: Db,
+    /// Dump import settings
+    pub import: Import,
+    /// RPC ports settings
+    pub ports: Ports,
 }
 
 /// Database settings
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Db {
-    url: String,
-    max_connections: u32,
-    connection_timeout: u64,
+    /// Path to database
+    pub url: String,
+    /// Number of maximum simultaneous connections
+    pub max_connections: u32,
+    /// DB connection timeout
+    pub connection_timeout: u64,
 }
 
 /// Anime index import settings
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Import {
-    new_download_path: String,
-    old_download_path: String,
-    new_extract_path: String,
-    old_extract_path: String,
+    /// Path to download new anime dump
+    pub new_download_path: String,
+    /// Path to download old anime dump
+    pub old_download_path: String,
+    /// Path to extract new anime dump
+    pub new_extract_path: String,
+    /// Path to extract old anime dump
+    pub old_extract_path: String,
 }
 
 /// gRPC services ports
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct Ports {
-    import: i32,
-    task: i32,
+    /// Port for `ImportService`
+    pub import: i32,
+    /// Port for `ScraperTasksService`
+    pub task: i32,
+}
+
+// MARK: impl Profile
+
+impl Profile {
+    fn files(&self) -> Vec<String> {
+        let mut files = vec!["config/default.toml".to_string()];
+        match self {
+            Profile::Default => {},
+            profile => files.push(format!("config/{}.toml", json!(profile))),
+        }
+
+        files
+    }
 }
 
 // MARK: impl Settings
 
 impl Settings {
-    pub fn new() -> Result<Self, ConfigError> {
+    pub fn new(profile: Profile) -> Result<Self, ConfigError> {
         let mut s = Config::new();
 
-        let default = File::with_name("config/default.toml");
-        s.merge(default)?;
+        for name in profile.files() {
+            let file = File::with_name(&name);
+            s.merge(file)?;
+        }
 
         s.try_into()
     }
 
-    /// Returns database settings
-    pub fn db(&self) -> &Db {
-        &self.db
-    }
+    pub fn db(&self) -> &Db { &self.db }
 
-    /// Returns dump import settings
-    pub fn import(&self) -> &Import {
-        &self.import
-    }
+    pub fn import(&self) -> &Import { &self.import }
 
-    /// Returns gRPC services ports settings
-    pub fn ports(&self) -> &Ports {
-        &self.ports
-    }
+    pub fn ports(&self) -> &Ports { &self.ports }
 }
 
 // MARK: impl Db
 
 impl Db {
-    /// Returns path to database
-    pub fn url(&self) -> &str {
-        &self.url
-    }
+    pub fn url(&self) -> &str { &self.url }
 
-    /// Returns number of maximum allowed db connections
-    pub fn max_connections(&self) -> u32 {
-        self.max_connections
-    }
+    pub fn max_connections(&self) -> u32 { self.max_connections }
 
-    /// Returns timeout for a db connection
     pub fn connection_timeout(&self) -> Duration {
         Duration::new(self.connection_timeout, 0)
     }
@@ -95,33 +121,19 @@ impl Db {
 // MARK: impl Import
 
 impl Import {
-    pub fn new_download_path(&self) -> &str {
-        &self.new_download_path
-    }
+    pub fn new_download_path(&self) -> &str { &self.new_download_path }
 
-    pub fn old_download_path(&self) -> &str {
-        &self.old_download_path
-    }
+    pub fn old_download_path(&self) -> &str { &self.old_download_path }
 
-    pub fn new_extract_path(&self) -> &str {
-        &self.new_extract_path
-    }
+    pub fn new_extract_path(&self) -> &str { &self.new_extract_path }
 
-    pub fn old_extract_path(&self) -> &str {
-        &self.old_extract_path
-    }
+    pub fn old_extract_path(&self) -> &str { &self.old_extract_path }
 }
 
 // MARK: impl Ports
 
 impl Ports {
-    /// Port for `ImportService`
-    pub fn import(&self) -> i32 {
-        self.import
-    }
+    pub fn import(&self) -> i32 { self.import }
 
-    /// Port for `ScraperTasksService`
-    pub fn task(&self) -> i32 {
-        self.task
-    }
+    pub fn task(&self) -> i32 { self.task }
 }
